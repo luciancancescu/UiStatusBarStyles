5b68b8c8933415e6bcd2dacfc1ccc31950d4cbc9
Lucian Cancescu <lucian.cancescu@gmail.com>
2015-03-06 01:36:10 +0100

    demo how to change the status of the UIStatusBar

--- UiStatusBarStyles/ViewController.swift
+++ UiStatusBarStyles/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentBackgroundColor = "light"
+    var statusBarIsVisible = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        switch currentBackgroundColor {
+        case "dark": return UIStatusBarStyle.LightContent
+        default: return UIStatusBarStyle.Default
+        }
     }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        UIStatusBarAnimation.Fade
+        if statusBarIsVisible {
+            return false
+        } else {
+            return true
+        }
+    }
+    
 
-
+    @IBAction func setWhiteBackground() {
+        self.view.backgroundColor = UIColor.whiteColor()
+        currentBackgroundColor = "light"
+        preferredStatusBarStyle()
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    
+    @IBAction func setDarkBackground() {
+        self.view.backgroundColor = UIColor.blackColor()
+        currentBackgroundColor = "dark"
+        preferredStatusBarStyle()
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    @IBAction func hideStatusBar() {
+        statusBarIsVisible = false
+        prefersStatusBarHidden()
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    @IBAction func showStatusBar() {
+        statusBarIsVisible = true
+        prefersStatusBarHidden()
+        setNeedsStatusBarAppearanceUpdate()
+    }
 }
 
